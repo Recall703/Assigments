50989b02e4edcd334dc8fbdbb7a5efa42fe5754a
Recall703 <monozuteoh0127@gmail.com>
2016-09-20 19:14:47 +0900

    Update zxcqwe.cpp

--- Degree_Y2S1/zxcqwe.cpp
+++ Degree_Y2S1/zxcqwe.cpp
@@ -1,8 +1,15 @@
+
 #include "SortAlgo.h"
 #include <iostream>
 using namespace std;
 
 int main(){
-	
-	return 0;
+	int anArray[]= {9,4,2,7,4};
+	SortAlgo <int> a;
+	a.BubbleSort(anArray,5);
+	for(int i=0;i<5;i++){
+		cout << anArray[i] << " ";
+	}
+
+
 }
