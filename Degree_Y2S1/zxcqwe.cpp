5984a302544cf95a5850d629603944006808e864
Recall703 <monozuteoh0127@gmail.com>
2016-09-20 19:25:35 +0900

    Update zxcqwe.cpp

--- Degree_Y2S1/zxcqwe.cpp
+++ Degree_Y2S1/zxcqwe.cpp
@@ -4,12 +4,32 @@
 using namespace std;
 
 int main(){
-	int anArray[]= {9,4,2,7,4};
-	SortAlgo <int> a;
-	a.BubbleSort(anArray,5);
+	int anArray[]= {9,4,2,7,5};
+	cout << "Bubble Sort(initial Array): ";
 	for(int i=0;i<5;i++){
 		cout << anArray[i] << " ";
 	}
-
+	cout << endl;
+	
+	SortAlgo <int> a;
+	a.BubbleSort(anArray,5);
+	
+	for(int i=0;i<5;i++){
+		cout << anArray[i] << " ";
+	}
+	
+	int anotherArray[] = {5, 6 ,7 ,3 ,4, 2};
+	cout << endl;
+	cout << "Selection Sort(Initial Array): ";
+	for(int i=0;i<5;i++){
+		cout << anotherArray[i] << " ";
+	}
+	a.SelectionSort(anotherArray,5);
+	cout << endl;
+	cout << "end of the Array: ";
+	for(int i=0;i<5;i++){
+		cout << anotherArray[i] << " ";
+	}
+	
 
 }
